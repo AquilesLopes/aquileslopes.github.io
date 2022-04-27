f62577398abd80c9ebc237a4f8c7dc94dd180899
Aquiles Lopes <aquiles_oliveira@live.com>
2022-04-27 08:43:31 -0300

    Add Tippy

---
+++ js/myTippy.js
@@ -0,0 +1,103 @@
+tippy('.devicon-html5-plain', {
+    content: 'HTML 5',
+});
+
+tippy('.devicon-css3-plain', {
+    content: 'CSS 3',
+});
+
+tippy('.devicon-javascript-plain', {
+    content: 'Javascript',
+});
+
+tippy('.devicon-typescript-plain', {
+    content: 'Typescript',
+});
+
+tippy('.devicon-java-plain', {
+    content: 'JAVA',
+});
+
+tippy('.devicon-python-plain', {
+    content: 'Python',
+});
+
+tippy('.devicon-r-plain', {
+    content: 'R Language',
+});
+
+tippy('.devicon-python-plain', {
+    content: 'Python',
+});
+
+tippy('.devicon-mongodb-plain', {
+    content: 'MongoDB',
+});
+
+tippy('.devicon-mysql-plain', {
+    content: 'MySQL',
+});
+
+tippy('.devicon-postgresql-plain', {
+    content: 'PostgreSQL',
+});
+
+tippy('.devicon-oracle-plain', {
+    content: 'Oracle',
+});
+
+tippy('.devicon-react-plain', {
+    content: 'React',
+});
+
+tippy('.devicon-ionic-original', {
+    content: 'Ionic',
+});
+
+tippy('.devicon-angularjs-plain', {
+    content: 'Angular',
+});
+
+tippy('.devicon-bootstrap-plain', {
+    content: 'Bootstrap',
+});
+
+tippy('.devicon-jquery-plain', {
+    content: 'jQuery',
+});
+
+tippy('.devicon-spring-plain', {
+    content: 'Spring Boot',
+});
+
+tippy('.devicon-vscode-plain', {
+    content: 'Visual Studio Code',
+});
+
+tippy('.devicon-rstudio-plain', {
+    content: 'RStudio',
+});
+
+tippy('.img-eclipse', {
+    content: 'Eclipse',
+});
+
+tippy('.devicon-ubuntu-plain', {
+    content: 'Ubuntu',
+});
+
+tippy('.devicon-git-plain', {
+    content: 'Git',
+});
+
+tippy('.devicon-github-original', {
+    content: 'GitHub',
+});
+
+tippy('.devicon-tomcat-line', {
+    content: 'Apache Tomcat',
+});
+
+tippy('.logo-linkedin', {
+    content: 'LinkedIn',
+});
