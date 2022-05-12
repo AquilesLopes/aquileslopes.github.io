00ebff016e120752803d8df34bd02231d118dc69
Aquiles Lopes <aquiles_oliveira@live.com>
2022-05-12 14:57:58 -0300

    update version 2.1

--- js/languageService.js
+++ js/languageService.js
@@ -1,4 +1,5 @@
 var PAGE_LOADED = false;
+var CURRENT_LANGUAGE = '';
 var LIST_PT_BR = [];
 var LIST_EN_UK = [];
 var SPINNER = '<span class="spinner-grow text-success spinner-grow-sm" role="status">';
@@ -14,38 +15,40 @@ $.getJSON('../language/en-UK.json', function(jd) {
 })
 
 function changeLanguage(language) {
-    var LIST_KEYS_VALUE = [];
+    if (CURRENT_LANGUAGE !== language) {
+        CURRENT_LANGUAGE = language;
+        var LIST_KEYS_VALUE = [];
 
-    if (language === 'pt-BR') {
-        LIST_KEYS_VALUE = LIST_PT_BR;
+        if (language === 'pt-BR') {
+            LIST_KEYS_VALUE = LIST_PT_BR;
 
-        var element = document.getElementById("img-brazil");
-        element.classList.remove("blackWhite");
+            var element = document.getElementById("img-brazil");
+            element.classList.remove("blackWhite");
 
-        element = document.getElementById("img-united-kingdom");
-        element.classList.add("blackWhite");
-    } else if (language === 'en-UK') {
-        LIST_KEYS_VALUE = LIST_EN_UK;
+            element = document.getElementById("img-united-kingdom");
+            element.classList.add("blackWhite");
+        } else if (language === 'en-UK') {
+            LIST_KEYS_VALUE = LIST_EN_UK;
 
-        var element = document.getElementById("img-united-kingdom");
-        element.classList.remove("blackWhite");
+            var element = document.getElementById("img-united-kingdom");
+            element.classList.remove("blackWhite");
 
-        element = document.getElementById("img-brazil");
-        element.classList.add("blackWhite");
-    }
-
-    if (PAGE_LOADED) {
-        for (var i = 0; i < LIST_KEYS_VALUE.length; i++) {
-            $("." + LIST_KEYS_VALUE[i].key).html(SPINNER);
+            element = document.getElementById("img-brazil");
+            element.classList.add("blackWhite");
         }
-        setTimeout(function() {
-            drawText(LIST_KEYS_VALUE)
-        }, 1000);
-    } else {
-        PAGE_LOADED = true;
-        drawText(LIST_KEYS_VALUE)
-    }
 
+        if (PAGE_LOADED) {
+            for (var i = 0; i < LIST_KEYS_VALUE.length; i++) {
+                $("." + LIST_KEYS_VALUE[i].key).html(SPINNER);
+            }
+            setTimeout(function() {
+                drawText(LIST_KEYS_VALUE)
+            }, 1000);
+        } else {
+            PAGE_LOADED = true;
+            drawText(LIST_KEYS_VALUE)
+        }
+    }
 }
 
 function drawText(LIST_KEYS_VALUE) {
